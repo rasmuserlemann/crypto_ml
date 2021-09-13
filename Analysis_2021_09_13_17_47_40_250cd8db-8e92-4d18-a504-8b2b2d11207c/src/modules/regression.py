f1c9f165fa55b508d0f0ca4e69aee33b7f1f376e
EC2 Default User <ec2-user@ip-172-31-30-50.ec2.internal>
2021-09-13 18:29:03 +0000

    works

---
+++ Analysis_2021_09_13_17_47_40_250cd8db-8e92-4d18-a504-8b2b2d11207c/src/modules/regression.py
@@ -0,0 +1,22 @@
+# Machine Learning and Regression Library for crypto analysis
+# Focus: Regression
+# Author: Rasmus Erlemann
+# Last Update: _
+
+#Basic Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#skikit Libraries
+
+class Regression:
+    def __init__(self, data0, method0):
+        self.data = data0
+        self.method = method0
+        if (self.method == "linear"):
+            self.linear = self.linear_()
+        #elif (self.method == "hierarchical"):
+            #self.cluster = self.hierarchical_()
+        else:
+            print("Regression method "+self.method+" Not Available")
+
